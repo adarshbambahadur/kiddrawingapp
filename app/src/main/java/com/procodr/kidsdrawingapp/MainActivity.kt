893a04d249ae4c0d25decf937fb4cc85785e73a9
Adarsh Singh <adarshbambahadur@gmail.com>
2023-08-29 15:38:18 +0530

    Brush Feature Added Need to fix error

--- app/src/main/java/com/procodr/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/procodr/kidsdrawingapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.procodr.kidsdrawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+    private var drawingView: DrawingView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawingView = findViewById(R.id.drawing_view)
+        drawingView?.setSizeForBrush(20.toFloat())
+
+        val ib_brush : ImageButton = findViewById(R.id.ib_brush)
+        ib_brush.setOnClickListener {
+            showBrushChooserDialog()
+        }
+    }
+
+    private fun showBrushChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+
+        val tinyBtn : ImageButton = findViewById(R.id.ib_tiny_brush)
+
+        brushDialog.show()
     }
 }
